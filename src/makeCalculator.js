217e8d8d835c3386a742f3b80fcc499717cd60ca
Gennadiy99 <login841@gmail.com>
2025-03-11 19:30:53 +0100

    finished task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,37 @@
 /**
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => {
+      if (b !== 0) {
+        return a / b;
+      } else {
+        throw new Error('Деление на ноль невозможно');
+      }
+    },
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function (callbeck, num1) {
+      this.result = callbeck(this.result, num1);
+
+      return this;
+    },
+  };
 }
+
+const Calculator = makeCalculator();
+
+Calculator.operate(Calculator.add, 21)
+  .operate(Calculator.subtract, 2)
+  .operate(Calculator.divide, 10);
 
 module.exports = makeCalculator;
